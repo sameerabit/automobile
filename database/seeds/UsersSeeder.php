151fbec51f55f182b3a9a0d0cca6e516e97db260
Sameera Viraj <sameera.emedia@gmail.com>
2020-05-07 16:56:33 +0530

    initial commit

---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'sameera',
+            'username' => 'admin',
+            'email' => 'virajamara@gmail.com',
+            'password' => Hash::make('admin'),
+        ]);
+    }
+}
