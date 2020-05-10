c3a68a46baa6dbc6365d6305abf6be33fed7ba24
Sameera Viraj <sameera.emedia@gmail.com>
2020-05-10 12:18:12 +0530

    supplier crud starting

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -15,7 +15,7 @@
                             <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
                           </div>
                           <div class="info">
-                            <a href="#" class="d-block">Alexander Pierce</a>
+                            <a href="#" class="d-block">{{ auth()->user()->name }}</a>
                           </div>
                         </div>
                   
@@ -24,58 +24,22 @@
                           <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                             <!-- Add icons to the links using the .nav-icon class
                                  with font-awesome or any other icon font library -->
-                            <li class="nav-item has-treeview menu-open">
-                              <a href="#" class="nav-link active">
-                                <i class="nav-icon fas fa-tachometer-alt"></i>
-                                <p>
-                                  Dashboard
-                                  <i class="right fas fa-angle-left"></i>
-                                </p>
-                              </a>
-                              <ul class="nav nav-treeview">
-                                <li class="nav-item">
-                                  <a href="./index.html" class="nav-link active">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Dashboard v1</p>
-                                  </a>
-                                </li>
-                                <li class="nav-item">
-                                  <a href="./index2.html" class="nav-link">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Dashboard v2</p>
-                                  </a>
-                                </li>
-                                <li class="nav-item">
-                                  <a href="./index3.html" class="nav-link">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Dashboard v3</p>
-                                  </a>
-                                </li>
-                              </ul>
-                            </li>
-                            <li class="nav-item">
-                              <a href="pages/widgets.html" class="nav-link">
-                                <i class="nav-icon fas fa-th"></i>
-                                <p>
-                                  Widgets
-                                  <span class="right badge badge-danger">New</span>
-                                </p>
-                              </a>
-                            </li>
+                            
+                            
                             <li class="nav-item has-treeview">
                               <a href="#" class="nav-link">
                                 <i class="nav-icon fas fa-copy"></i>
                                 <p>
-                                  Layout Options
+                                  Inventory
                                   <i class="fas fa-angle-left right"></i>
-                                  <span class="badge badge-info right">6</span>
+                                  <span class="badge badge-info right"></span>
                                 </p>
                               </a>
                               <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                  <a href="pages/layout/top-nav.html" class="nav-link">
+                                  <a href="{{ route('suppliers.index') }}" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
-                                    <p>Top Navigation</p>
+                                    <p>Supplier</p>
                                   </a>
                                 </li>
                                 <li class="nav-item">
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
+
+Route::resource('suppliers', 'SupplierController');
+
